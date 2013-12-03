fb5a1e0c38be915434b4b3041d24ed0d4ee7fdc1
Fabian Ström <fs@fabianstrom.se>
2013-12-03 19:58:55 +0100

    reading fourier series instead of coefficients for a single sinus function

--- christmasleds.ino
+++ christmasleds.ino
@@ -1,21 +1,8 @@
 #include <SPI.h>
 #include <Ethernet.h>
 
-// enable or disable serial debugging
-#define SER 0
-
-// LED:s
-#define RED 9
-#define GREEN 5
-#define BLUE 6
-
-// pi and 2*pi
-#define PI  3.141592654
-#define PI2 (PI*2)
-
-// network stuff
-#define PORT 8467
-#define BUFFSIZE 17
+#include "christmasleds.h"
+#include "debug.h"
 
 byte mac[] = {0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02};
 byte ip[] = {192, 168, 1, 4};
@@ -24,160 +11,141 @@ byte subnet[] = {255, 255, 255, 0};
 
 EthernetServer server = EthernetServer(PORT);
 
-/* 
- * An abstract description of
- * the colour function that is
- * used.
- * y(x):=A*sin(B/c*x+D)+E
- */
-struct colourFunction {
-	unsigned char a;
-	unsigned char b;
-	unsigned char c;
-	float bc; // bc = B / C
-	unsigned char d;
-	unsigned char e;
-};
-
-/*
- * A program is defined by
- * three colour functions and
- * the time between each iteration.
- */
-struct colourSettings {
-	unsigned char waittime;
-	struct colourFunction red;
-	struct colourFunction green;
-	struct colourFunction blue;
-};
-
-
-struct colourSettings cs;
-int x = 0;
-byte buff[BUFFSIZE];
+struct colourSettingsFourier csf;
 
 void setup()
 {
 	Ethernet.begin(mac, ip, gateway, subnet);
-#if SER
-	Serial.begin(9600);
-	Serial.println(Ethernet.localIP()); 
-#endif
+	DEBUG(
+		Serial.begin(9600);
+		Serial.println(Ethernet.localIP()); 
+	);
 	selfTest();  
 	selfTest();  
 	server.begin();
 
 	/* set a default program */
-	cs.waittime = 10;
-	cs.red.a = cs.green.a = cs.blue.a = 0;
-	cs.red.b = cs.green.b = cs.blue.b = 22;
-	cs.red.c = cs.green.c = cs.blue.c = 255;
-	cs.red.d = cs.green.d = cs.blue.d = 0;
-	cs.red.e = cs.green.e = cs.blue.e = 255;
+	csf.red.a0 = 128;
+	csf.red.N = 1;
+	csf.red.an = (float *) calloc(1, sizeof(float));
+	csf.red.an[0] = 0;
+	csf.red.bn = (float *) calloc(1, sizeof(float));
+	csf.red.bn[0] = 1;
 
-	/* set buffer to 0 */
-	memset(buff, 0, BUFFSIZE);
+	csf.green.a0 = 128;
+	csf.green.N = 1;
+	csf.green.an = (float *) calloc(1, sizeof(float));
+	csf.green.an[0] = 0;
+	csf.green.bn = (float *) calloc(1, sizeof(float));
+	csf.green.bn[0] = 1;
+
+	csf.blue.a0 = 128;
+	csf.blue.N = 1;
+	csf.blue.an = (float *) calloc(1, sizeof(float));
+	csf.blue.an[0] = 0;
+	csf.blue.bn = (float *) calloc(1, sizeof(float));
+	csf.blue.bn[0] = 1;
+	
+	csf.waittime = 500;
+	csf.period = 6.28;
+	csf.xstep = 0.01;
+	csf.x = 0;
+
+	DEBUG(
+		printFourierSettings(&csf);
+		Serial.println("Ready");
+	);
 }
 
-
-unsigned char fun(unsigned char a, unsigned char bc, unsigned char d, 
-		unsigned char e) 
-{
-	return a*sin(bc+d)+e;
-}
 
 void loop()
 {
 	EthernetClient client = server.available();
 	if (client == true) { /* got connection, read and parse data */
-		parsePacketIntoSettings(client);
-#if SER
-		printSettings();
-#endif
+		/* the first byte is the protocol version identifier */
+		unsigned char version = client.parseInt();
+		parseFourier(client);
+		DEBUG(Serial.println(version);printFourierSettings(&csf););
 	} else { /* there is no data available, update instead */
-		unsigned char r = fun(cs.red.a, cs.red.bc, cs.red.d, cs.red.e);
-		unsigned char g = fun(cs.green.a, cs.green.bc, cs.green.d, cs.green.e);
-		unsigned char b = fun(cs.blue.a, cs.blue.bc, cs.blue.d, cs.blue.e);
-		analogWrite(RED, r);
-		analogWrite(GREEN, g);
-		analogWrite(BLUE, b);
-#if SER
-		//  Serial.println(cs.green.a*sin(x*cs.green.bc+cs.green.d)+cs.green.e);
-#endif
-		x++;
-		delay(cs.waittime);
+		updateFourier();
+		csf.x += csf.xstep;
+
+		if (csf.x > csf.period) {
+			csf.x = 0;
+		}
 	}
 }
 
-void parsePacketIntoSettings(EthernetClient client) 
+void updateFourier()
 {
-	float b,c;
-
-	/* read and discard the first byte */
-	(void) client.read(); 
-
-	/* read data */
-	cs.waittime = client.read();
-	cs.red.a = client.read();
-	cs.red.b = client.read();
-	cs.red.c = client.read();
-	cs.red.d = client.read();
-	cs.red.e = client.read();
-	cs.green.a = client.read();
-	cs.green.b = client.read();
-	cs.green.c = client.read();
-	cs.green.d = client.read();
-	cs.green.e = client.read();
-	cs.blue.a = client.read();
-	cs.blue.b = client.read();
-	cs.blue.c = client.read();
-	cs.blue.d = client.read();
-	cs.blue.e = client.read();
-
-	/* create bc = b/c */
-	b = (float) cs.red.b;
-	c = (float) cs.red.c;
-	cs.red.bc = b/c;
-	b = (float) cs.green.b;
-	c = (float) cs.green.c;
-	cs.green.bc = b/c;
-	b = (float) cs.blue.b;
-	c = (float) cs.blue.c;
-	cs.blue.bc = b/c;
-#if SER
-	Serial.println(cs.red.bc);
-	Serial.println(cs.green.bc);
-	Serial.println(cs.blue.bc);
-#endif
+	DEBUG(Serial.print("updating fourier "); Serial.println(csf.x););
+	unsigned char r = fourier(&csf.red);
+	unsigned char g = fourier(&csf.green);
+	unsigned char b = fourier(&csf.blue);
+	analogWrite(RED, r);
+	analogWrite(GREEN, g);
+	analogWrite(BLUE, b);
+	DEBUG(
+		Serial.print("r: "); Serial.println(r);
+		Serial.print("g: "); Serial.println(g);
+		Serial.print("b: "); Serial.println(b);
+        );
+	delay(csf.waittime);
 }
-#if SER
-void printSettings()
+
+void parseFourier(EthernetClient client)
 {
-	Serial.println("****************************************");
-	Serial.print("Waittime: ");
-	Serial.print(cs.waittime, DEC);
-	Serial.println(" ms");
-	Serial.print("Red:   r(t)=");
-	Serial.print(cs.red.a, DEC); Serial.print("*sin(");
-	Serial.print(cs.red.b, DEC); Serial.print("*t/");
-	Serial.print(cs.red.c, DEC); Serial.print("+");
-	Serial.print(cs.red.d, DEC); Serial.print(")+");
-	Serial.print(cs.red.e, DEC); Serial.print("\n");
-	Serial.print("Green: g(t)=");
-	Serial.print(cs.green.a, DEC); Serial.print("*sin(");
-	Serial.print(cs.green.b, DEC); Serial.print("/");
-	Serial.print(cs.green.c, DEC); Serial.print("*t+");
-	Serial.print(cs.green.d, DEC); Serial.print(")+");
-	Serial.print(cs.green.e, DEC); Serial.print("\n");
-	Serial.print("Blue:  b(t)=");
-	Serial.print(cs.blue.a, DEC); Serial.print("*sin(");
-	Serial.print(cs.blue.b, DEC); Serial.print("/");
-	Serial.print(cs.blue.c, DEC); Serial.print("*t+");
-	Serial.print(cs.blue.e, DEC); Serial.print(")+");
-	Serial.print(cs.blue.e, DEC); Serial.print("\n");
-	Serial.println("****************************************");
+	DEBUG(
+		Serial.println("Parsing fourier");
+		long time = millis();
+	);
+
+	/* read waittime, step and period */
+	csf.waittime = client.parseInt();
+	csf.xstep = client.parseFloat();
+	csf.period = client.parseFloat();
+	
+	/* read red */
+	csf.red.a0 = client.parseFloat();
+	csf.red.N = client.parseInt();
+	free(csf.red.an);
+	free(csf.red.bn);
+	csf.red.an = (float *) calloc(csf.red.N, sizeof(float));
+	csf.red.bn = (float *) calloc(csf.red.N, sizeof(float));
+	for(int i=0;i<csf.red.N;i++){csf.red.an[i]=client.parseFloat();}
+	for(int i=0;i<csf.red.N;i++){csf.red.bn[i]=client.parseFloat();}
+	
+	/* read green */
+	csf.green.a0 = client.parseFloat();
+	csf.green.N = client.parseInt();
+	free(csf.green.an);
+	free(csf.green.bn);
+	csf.green.an = (float *) calloc(csf.green.N, sizeof(float));
+	csf.green.bn = (float *) calloc(csf.green.N, sizeof(float));
+	for(int i=0;i<csf.green.N;i++){csf.green.an[i]=client.parseFloat();}
+	for(int i=0;i<csf.green.N;i++){csf.green.bn[i]=client.parseFloat();}
+	
+	/* read blue */
+	csf.blue.a0 = client.parseFloat();
+	csf.blue.N = client.parseInt();
+	free(csf.blue.an);
+	free(csf.blue.bn);
+	csf.blue.an = (float *) calloc(csf.blue.N, sizeof(float));
+	csf.blue.bn = (float *) calloc(csf.blue.N, sizeof(float));
+	for(int i=0;i<csf.blue.N;i++){csf.blue.an[i]=client.parseFloat();}
+	for(int i=0;i<csf.blue.N;i++){csf.blue.bn[i]=client.parseFloat();}
+
+	/* we have read what we wanted, discard the rest data (if any) */
+	client.flush();
+
+	DEBUG(
+		long diff = millis() - time;
+		Serial.println("Fourier parsed: ");
+		Serial.print(diff);
+		Serial.println(" ms");
+	);
 }
-#endif
+
 void selfTest()
 {
 	/* make sure the leds are off */
@@ -198,29 +166,27 @@ void selfTest()
 
 /*
  * Compute the fourier series in the point x.
- * an[] An array of size N with the an coefficients 
- * bn[] An array of size N with the bn coefficients 
  * See http://en.wikipedia.org/wiki/Fourier_series.
  */
-int fourier(int x, float an[], float bn[], float a0, int N, float P)
+int fourier(struct fourierSeries *col)
 {
-	float val = a0;
-	float pre = PI2*x/P;
+	float val = 0;
+	float pre = PI2*csf.x/csf.period;
 
-#ifdef SER
-	unsigned long time = micros();
-#endif
+	DEBUG(unsigned long time = micros(););
 	
-	for (int i=1; i<N; i++) {
-		val += an[i-1]*cos(pre*i) + bn[i-1]*sin(pre*i);
+	for (int i=1; i<=col->N; i++) {
+		val += col->an[i-1]*cos(pre*i) + col->bn[i-1]*sin(pre*i);
 	}
 
-#ifdef SER
-	unsigned long diff = micros() - time;
-	Serial.print("Fourier took ");
-	Serial.print(diff, DEC);
-	Serial.println(" micro seconds");
-#endif
+	val = val*128+col->a0;
+
+	DEBUG(
+		unsigned long diff = micros() - time;
+		Serial.print("Fourier took ");
+		Serial.print(diff, DEC);
+		Serial.println(" microseconds");
+	);
 
 	return (int) val;
 }
